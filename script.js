d56eba012dc91d0727621f897d8d3fa01c83166e
3Clele31 <168022820+3Clele31@users.noreply.github.com>
2024-09-18 11:56:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const avanca = document.querySelectorAll('.btn-proximo');
-Leonardo Lima Pimentel 21.08.2024
 avanca.forEach(button => {
     button.addEventListener('click',function(){
         const atual = document.querySelector('.ativo');
